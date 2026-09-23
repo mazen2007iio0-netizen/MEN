635da5b5d6edf246b7b34b2ae6158186dce6ff4d
mazen2007iio0-netizen <mazen2007iio0@gmail.com>
2026-09-23 05:35:55 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://xoqwzluyxynqpdpmidts.supabase.co";
+const SUPABASE_KEY = "sb_publishable_rQvBPw08M9Q3bWTDfFseTQ_6SU3aN96";
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
